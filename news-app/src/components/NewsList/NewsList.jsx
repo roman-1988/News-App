05bf67a45ac0a0b1d643b3240f6454839ab1f8f6
Roman <roman-1@inbox.ru>
2024-03-24 21:11:10 +0300

    Перенес каждую отдельно отображаемую новость из списка новостей в отдельный компонент NewsItem и добавил к ним для каждой отображение картинки. Применил стили.

--- news-app/src/components/NewsList/NewsList.jsx
+++ news-app/src/components/NewsList/NewsList.jsx
@@ -1,11 +1,12 @@
+import { NewsItem } from "../NewsItem/NewsItem"
 import styles from "./styles.module.css"
 
 const NewsList = ({ news }) => {
-    
+
     return (
         <ul className={styles.list}>
             {news.map((item) => {
-                return <li key={item.id}>{item.title}</li>
+                return <NewsItem key={item.id} item={item} />
             })}
         </ul>
     )
